5338f60d0d03fa8e5508c51a5fd8f44f4722adb6
Felipe S <rfelipe273@gmail.com>
2021-12-01 00:43:33 -0300

    ensure LoginRouter return a valid token when valid credentials are provided.

--- src/presentation/routers/login_router.spec.js
+++ src/presentation/routers/login_router.spec.js
@@ -72,7 +72,7 @@ describe('Login Router', () => {
     });
 
     test('Should return 200 if valid credentials are provided', () => {
-        let { sut } = makeSut();
+        let { sut, authUseCaseSpy } = makeSut();
         let httpRequest = {
             body: {
                 email: 'valid_email@email.com',
@@ -81,6 +81,7 @@ describe('Login Router', () => {
         };
         let httpResponse = sut.route(httpRequest);
         expect(httpResponse.statusCode).toBe(200);
+        expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken);
     });
 
     test('Should return 401 when invalid credentials are provided', () => {
