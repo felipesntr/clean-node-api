85337b34615f84a0e0745c6663c4d36605bd31e5
Felipe S <rfelipe273@gmail.com>
2021-11-30 22:17:24 -0300

    ensure LoginRouter returns 400 if no password is provided.

--- src/presentation/routers/login_router.spec.ts
+++ src/presentation/routers/login_router.spec.ts
@@ -1,7 +1,7 @@
 
 class LoginRouter {
     route(httpRequest) {
-        if (!httpRequest.email) {
+        if (!httpRequest.email || !httpRequest.password) {
             return {
                 statusCode: 400,
             };
@@ -19,5 +19,16 @@ describe('Login Router', () => {
         }
         let httpResponse = sut.route(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
-    })
+    });
+
+    test('Should return 400 if no password is provided', () => {
+        let sut = new LoginRouter();
+        let httpRequest = {
+            body: {
+                email: 'any_email@email.com'
+            }
+        }
+        let httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
 })
