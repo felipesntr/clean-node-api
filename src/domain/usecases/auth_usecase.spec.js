5c35cf495c8fbad726fb40477dafe31541320832
Felipe S <rfelipe273@gmail.com>
2021-12-04 22:14:06 -0300

    ensure AuthUseCase throws if no dependency is provided.

--- src/domain/usecases/auth_usecase.spec.js
+++ src/domain/usecases/auth_usecase.spec.js
@@ -166,6 +166,7 @@ describe('Auth UseCase', () => {
         const invalid = {};
         const loadUserByEmailRepository = makeLoadUserByEmailRepository();
         const encrypter = makeEncrypter();
+        const tokenGenerator = makeTokenGenerator();
         const suts = [].concat(
             new AuthUseCase(),
             new AuthUseCase({
@@ -191,6 +192,18 @@ describe('Auth UseCase', () => {
                 loadUserByEmailRepository,
                 encrypter,
                 tokenGenerator: invalid,
+            }),
+            new AuthUseCase({
+                loadUserByEmailRepository,
+                encrypter,
+                tokenGenerator,
+                updateAccessTokenRepository: null,
+            }),
+            new AuthUseCase({
+                loadUserByEmailRepository,
+                encrypter,
+                tokenGenerator,
+                updateAccessTokenRepository: invalid,
             })
         );
 
