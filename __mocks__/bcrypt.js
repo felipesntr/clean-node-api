5581b1204af3016ea20edff909ba18deb9fab24a
Felipe S <rfelipe273@gmail.com>
2021-12-04 22:34:27 -0300

    ensure Encrypter returns false if bcrypt returns false;

---
+++ __mocks__/bcrypt.js
@@ -0,0 +1,7 @@
+module.exports = {
+    is_valid: true,
+
+    async compare(string, hashed_string) {
+        return this.is_valid;
+    }
+}
--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,17 +1,24 @@
-
+const bcrypt = require('bcrypt');
 class Encrypter {
-    async compare(password, hashed_password) {
-        return true;
+    async compare(string, hashed_string) {
+        const is_valid = await bcrypt.compare(string, hashed_string);
+        return is_valid;
     }
 }
-
 
 describe('Encrypter', () => {
 
     test('Should return true if bcrypt return trues', async () => {
         const sut = new Encrypter();
-        const is_valid = await sut.compare('any_password', 'hashed_password');
+        const is_valid = await sut.compare('string', 'hashed_string');
         expect(is_valid).toBe(true);
+    });
+
+    test('Should return false if bcrypt return false', async () => {
+        const sut = new Encrypter();
+        bcrypt.is_valid = false;
+        const is_valid = await sut.compare('string', 'hashed_string');
+        expect(is_valid).toBe(false);
     });
 
 });
