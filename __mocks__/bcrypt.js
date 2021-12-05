7541698c4d2a8b3dab2d9b7102effe25c168bafd
Felipe S <rfelipe273@gmail.com>
2021-12-04 22:39:27 -0300

    move sut creation to a factory helper method.

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,7 +1,10 @@
 module.exports = {
     is_valid: true,
-
+    string: '',
+    hashed_string: '',
     async compare(string, hashed_string) {
+        this.string = string;
+        this.hashed_string = hashed_string;
         return this.is_valid;
     }
 }
--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -6,19 +6,31 @@ class Encrypter {
     }
 }
 
+const makeSut = () => {
+    return new Encrypter();
+}
+
 describe('Encrypter', () => {
 
     test('Should return true if bcrypt return trues', async () => {
-        const sut = new Encrypter();
+        const sut = makeSut();
         const is_valid = await sut.compare('string', 'hashed_string');
         expect(is_valid).toBe(true);
     });
 
     test('Should return false if bcrypt return false', async () => {
-        const sut = new Encrypter();
+        const sut = makeSut();
         bcrypt.is_valid = false;
         const is_valid = await sut.compare('string', 'hashed_string');
         expect(is_valid).toBe(false);
+    });
+
+    test('Should calls bcrypt with correct values', async () => {
+        const sut = makeSut();
+        bcrypt.is_valid = false;
+        const is_valid = await sut.compare('string', 'hashed_string');
+        expect(bcrypt.string).toBe('string');
+        expect(bcrypt.hashed_string).toBe('hashed_string');
     });
 
 });
