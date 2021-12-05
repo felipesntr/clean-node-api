40138bc021e1440df9907a732980bc86b82b95d0
Felipe S <rfelipe273@gmail.com>
2021-12-05 04:45:08 -0300

    move mongodb setup to a separated file.

---
+++ src/infra/helpers/mongo_helper.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb');
+
+module.exports = {
+    async connect(url, db_name = undefined) {
+        this.client = await MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        this.db = this.client.db(db_name);
+        this.db_name = db_name;
+    },
+
+    async disconnect() {
+        await this.client.close();
+    }
+}
